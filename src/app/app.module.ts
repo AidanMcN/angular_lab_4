f2a0a259289050b27c6274d4a1f9c5825cca86a5
aidan mcnea <aidanmcnea@gmail.com>
2021-02-24 19:27:31 +0000

    Lab Upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponentaComponent } from './componenta/componenta.component';
+import { ComonentbComponent } from './comonentb/comonentb.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentaComponent,
+    ComonentbComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/comonentb/comonentb.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comonentb',
+  templateUrl: './comonentb.component.html',
+  styleUrls: ['./comonentb.component.css']
+})
+export class ComonentbComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
